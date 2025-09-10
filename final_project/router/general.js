ce5558627cc6f7b87206f9b3740daecbbad32863
Paulo Moreira <moreira.pjb@gmail.com>
2025-09-10 08:42:27 -0400

    Task 3 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = [];
+  for (const bookId in books) {
+      if (books[bookId].author === author) {
+          booksByAuthor.push(books[bookId]);
+      }
+  }
+  if (booksByAuthor.length > 0) {
+      return res.json(booksByAuthor);
+  } else {
+      return res.status(404).json({message: "No books were found for this author"});
+  }
 });
 
 // Get all books based on title
